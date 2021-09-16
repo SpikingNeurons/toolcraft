d6d26438073bce9c3a16c92bcabaf8ee7f23da4b
SpikingNeuron <praveenneuron@gmail.com>
2021-09-16 14:24:18 +0200

    trying to setup netlify with websit

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -59,6 +59,9 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
             label: 'GitHub',
             position: 'right',
           },
+          // {
+          //   type: 'docsVersionDropdown',
+          // },
         ],
       },
       footer: {
