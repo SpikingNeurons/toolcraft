9b9f897e393369a58ac7ff4b701fa99c683de8fc
Praveen Kulkarni <praveenneuron@gmail.com>
2021-09-20 04:45:37 +0200

    supporting bumpversion

--- tasks.py
+++ tasks.py
@@ -94,4 +94,9 @@ def doc_preview(c):
 
 )
 def bumpversion(c):
+    """
+    Easy way to test `bump2version`
+      bump2version --no-configured-files --dry-run
+      --current-version 0.1.2alpha22 num --verbose
+    """
     ...
