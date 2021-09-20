ba0c310603732a320e4956bc8315b58bbebb2a3b
Praveen Kulkarni <praveenneuron@gmail.com>
2021-09-20 07:57:27 +0200

    supporting bumpversion

--- tasks.py
+++ tasks.py
@@ -91,23 +91,94 @@ def doc_preview(c):
 
 
 @task(
-
+    help={
+        'show_version': 'Display current version',
+        'alpha': 'Make alpha release',
+        'beta': 'Make beta release',
+        'patch': 'Make patch release',
+        'minor': 'Make minor release',
+        'major': 'Make major release',
+    }
 )
-def bump(c):
+def bump(
+    c, show_version=False,
+    alpha=False, beta=False, patch=False, minor=False, major=False,
+):
     """
-    Format is:
-      >> {major}.{minor}.{patch}{release}{num}\
-
-    Easy way to test `bump2version`
-      bump2version --no-configured-files --dry-run --verbose
-      --current-version 0.1.2alpha22 num
-        >> 'Bump version: 0.1.2alpha22 → 0.1.2alpha23'
-      --current-version 0.1.2beta22 release
-        >> 'Bump version: 0.1.2beta22 → 0.1.2'
-      --current-version 0.1.2alpha22 patch
-        >> 'Bump version: 0.1.2alpha22 → 0.1.3'
-      --current-version 0.1.2 patch
-        >> 'Bump version: 0.1.2 → 0.1.3alpha0'
-
+    Handle bumping versions for toolcraft library.
+    Version style
+      >> {major}.{minor}.{patch}{release_type}{release_num}
+        release_type is one of ['a', 'b']
+      >> {major}.{minor}.{patch}
     """
-    _curr_ver = toml.load("pyproject.toml")['tool']['poetry']['version']
+    # ------------------------------------------------- 01
+    # Format is:
+    #   >> {major}.{minor}.{patch}{release}{num}\
+    #
+    # Easy way to test `bump2version`
+    #   bump2version --no-configured-files --dry-run --verbose
+    #     --current-version 0.1.2 --new-version 0.1.2a2 xyz
+
+    # ------------------------------------------------- 02
+    # detect current version
+    _curr_ver = \
+        toml.load("pyproject.toml")['tool']['poetry']['version'].split(".")
+    _major, _minor, _patch = \
+        int(_curr_ver[0]), int(_curr_ver[1]), _curr_ver[2]
+    _release_type = None
+    _release_num = None
+    if _patch.find("a") > -1:
+        _release_type = "a"
+        _release_num = int(_patch.split("a")[1])
+        _patch = int(_patch.split("a")[0])
+    elif _patch.find("b") > -1:
+        _release_type = "b"
+        _release_num = int(_patch.split("b")[1])
+        _patch = int(_patch.split("b")[0])
+    else:
+        _patch = int(_patch)
+
+    # ------------------------------------------------- 03
+    # show version
+    if show_version:
+        print(
+            "Current version is",
+            {
+                "major": _major,
+                "minor": _minor,
+                "patch": _patch,
+                "release_type": _release_type,
+                "release_num": _release_num,
+            }
+        )
+        return
+
+    # ------------------------------------------------- 04
+    # figure out new version
+    # ------------------------------------------------- 04.01
+    # if alpha release
+    if alpha:
+        # if current release is stable then increment patch and add alpha tag
+        if _release_type is None:
+            _patch += 1
+            _release_type = "a"
+            _release_num = 0
+        elif _release_type == "a":
+            _release_num += 1
+        elif _release_type == "b":
+            raise Exception(
+                "Current release is in beta so alpha release is not possible"
+            )
+        else:
+            raise Exception("Should not happen ...")
+
+    # ------------------------------------------------- 05
+    _new_ver = f"{_major}.{_minor}.{_patch}" \
+               f"{_release_type or ''}{_release_num or ''}"
+    _bump_command = f"bump2version --verbose --new-version {_new_ver}"
+    _run(c, f'{_bump_command} '
+            f'--search version = "{_curr_ver}" '
+            f'--replace version = "{_new_ver} xyz pyproject.toml"')
+
+
+
