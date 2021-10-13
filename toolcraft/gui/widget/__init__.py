d1931df2d688bf141cd17a343a67be4a1b93a40a
Praveen Kulkarni <praveenneuron@gmail.com>
2021-10-13 14:52:50 +0200

    make location a PlotLocation enum

--- toolcraft/gui/widget/__init__.py
+++ toolcraft/gui/widget/__init__.py
@@ -4,9 +4,4 @@ import typing as t
 PLOT_DATA_TYPE = t.Union[t.List[float], np.ndarray]
 
 from .core import Table, Plot
-from .auto import Window, SubPlot, DragLine, DragPoint, Child
-from .auto import TableSizing, PlotColormap, PlotMarker
-from .auto import Row, Column, Legend, XAxis, YAxis, ColorMapScale
-from .auto import Group, Tab, TabBar, TabButton, Combo, Button, Text, Separator
-from .auto import CheckBox, InputIntX, InputInt, CollapsingHeader, InSameLine
-from .auto import SliderInt, SliderIntX, SliderFloat, SliderFloatX, Slider3D
+from .auto import *
