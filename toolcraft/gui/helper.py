5441baf638f54768fd3220ff99fba83ec3d2d0b5
Praveen Kulkarni <praveenneuron@gmail.com>
2021-11-10 23:48:13 +0100

    yyyyy

--- toolcraft/gui/helper.py
+++ toolcraft/gui/helper.py
@@ -5,17 +5,13 @@ from .. import marshalling as m
 from .. import error as e
 
 
-def tab_bar_from_widget_dict(
-    widget_dict: t.Dict, parent: widget.ContainerWidget
-):
+def tab_bar_from_widget_dict(widget_dict: t.Dict) -> widget.TabBar:
     _tab_bar = widget.TabBar()
-    parent(widget=_tab_bar)
-    for _k in widget_dict.keys():
+    for _k, _v in widget_dict.items():
         _tab = widget.Tab(label=_k)
         _tab_bar(widget=_tab)
-        _v = widget_dict[_k]
         if isinstance(_v, dict):
-            tab_bar_from_widget_dict(_v, _tab)
+            _tab(tab_bar_from_widget_dict(_v))
         elif isinstance(_v, list):
             for _i, __v in enumerate(_v):
                 _tab(widget=__v)
@@ -25,21 +21,21 @@ def tab_bar_from_widget_dict(
                     f"Unrecognized type {type(_v)}"
                 ]
             )
+    return _tab_bar
 
 
 def tab_bar_from_hashable_callables(
     title: str,
     hashable: m.HashableClass,
     callable_names: t.Dict[str, str],
-):
+) -> widget.TabBar:
     # tab bar
-    _tab_bar = widget.TabBar(
-        label=title
-    )
-
+    _tab_bar = widget.TabBar(label=title)
     # loop over callable names
     for k, v in callable_names.items():
         _ret_widget = getattr(hashable, v)()
         _tab = widget.Tab(label=k)
         _tab(widget=_ret_widget)
         _tab_bar(widget=_tab)
+    # return
+    return _tab_bar
