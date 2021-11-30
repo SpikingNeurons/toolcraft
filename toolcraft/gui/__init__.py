9cdaa324994e3c20ced23964cdef3185672f5c95
Praveen Kulkarni <praveenneuron@gmail.com>
2021-11-30 23:26:36 +0100

    info_widget

--- toolcraft/gui/__init__.py
+++ toolcraft/gui/__init__.py
@@ -1,3 +1,9 @@
+"""
+todo: Separate gui module in new package so that
+  + there are few dependencies ... like dearpygui and on rest/grpc api access library
+  + can be deployed as exe using pyinstaller, Nuitka
+"""
+
 from .__base__ import COLOR_TYPE, PLOT_DATA_TYPE, USER_DATA, Tag
 from ._enum import *
 from . import window
