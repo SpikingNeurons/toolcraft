120135ed6ab601acfa696c9cfed7dfbd36fdd7c1
Praveen Kulkarni <praveenneuron@gmail.com>
2022-01-13 19:27:30 +0100

    add extra error log to show file names for config load errors

--- toolcraft/__init__.py
+++ toolcraft/__init__.py
@@ -38,7 +38,7 @@ __author__ = """Praveen Kulkarni"""
 __email__ = "praveenneuron@gmail.com"
 __version__ = "0.1.4a4"
 
-from . import logger, settings, marshalling, util, error, helper, parallel, storage
+from . import logger, settings, marshalling, util, error, helper, parallel, storage, job
 
 if settings.DPG_WORKS:
     # as some rules use dpg
