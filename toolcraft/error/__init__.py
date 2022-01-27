3e735e6b33c8b0963b9d2ba7f523cbb43cf8f49b
Praveen Kulkarni <praveenneuron@gmail.com>
2022-01-27 13:22:49 +0100

    heavy untested changes

--- toolcraft/error/__init__.py
+++ toolcraft/error/__init__.py
@@ -1,2 +1,7 @@
+"""
+todo: just like typer.Abort() we will implement errors taht can eb raised so that we
+  need to explicitly type raise at end of error
+"""
+
 from .__base__ import CustomException
 from . import code, io, validation
