53401f3f993adfff4f3d42b79bdf583e4f9b9e01
Praveen Kulkarni <praveenneuron@gmail.com>
2022-01-30 17:18:08 +0100

    rrrrrrrrrrrrrrrrrrrrrr

--- toolcraft/error/_script.py
+++ toolcraft/error/_script.py
@@ -44,7 +44,6 @@ def add_raise_before_exceptions_to_be_raised_explicitly():
                         _tokens.append(_token)
             except TypeError:
                 ...
-    _raise_if_failed_str = ".raise_if_failed"
     for _fo in [
         pathlib.Path("C:\\Users\\prave\\Documents\\Github\\RU"),
         pathlib.Path("C:\\Users\\prave\\Documents\\Github\\toolcraft"),
@@ -57,7 +56,7 @@ def add_raise_before_exceptions_to_be_raised_explicitly():
             print("Processing", _fi)
             # read source
             _src_txt = _fi.read_text(encoding="utf8")
-            # make sure that raise_if_failed is not at end
+            # make sure that raise_if_failed is not at end i.e. by detecting "."
             for _t in _tokens:
                 _r_t = _t.replace(".", "\.") + "[\s\S]*?\][\s\S]*?\)"
                 for _match in re.finditer(_r_t, string=_src_txt):
@@ -79,9 +78,76 @@ def add_raise_before_exceptions_to_be_raised_explicitly():
             _fi.write_text(_src_txt, encoding="utf8")
 
 
+def add_raise_if_expected_after_some_exceptions():
+    from toolcraft.error import code, io, validation
+    from toolcraft.error import __base__
+    _tokens = []
+    for _m in [code, io, validation]:
+        for _a in dir(_m):
+            _v = getattr(_m, _a)
+            try:
+                if issubclass(_v, __base__._CustomException):
+                    _r = inspect.getsource(_v.__init__).find("return")
+                    # raise_if_expected needed for exceptions that could not be _RAISE_EXPLICITLY
+                    if not _v._RAISE_EXPLICITLY:
+                        _token = f"{_v.__module__}.{_v.__name__}".replace("toolcraft.error", "e")
+                        print(_v, _token)
+                        _tokens.append(_token)
+            except TypeError:
+                ...
+    for _fo in [
+        pathlib.Path("C:\\Users\\prave\\Documents\\Github\\RU"),
+        pathlib.Path("C:\\Users\\prave\\Documents\\Github\\toolcraft"),
+    ]:
+        for _fi in _fo.glob('**/*.py'):
+            # do not apply anything for code in this file
+            if _fi.absolute() == pathlib.Path(__file__).absolute():
+                continue
+            # log
+            print("Processing", _fi)
+            # read source
+            _src_txt = _fi.read_text(encoding="utf8")
+            # make sure that raise is not at start
+            for _t in _tokens:
+                if _src_txt.find(f"raise {_t}") != -1:
+                    raise Exception(
+                        f"You have explicitly raised exception {_t} in file {_fi}. "
+                        f"Please instead call `raise_if_failed()` method on "
+                        f"exception instance ... "
+                    )
+            # find the end location for matched tokens
+            _locations_to_inject = []
+            for _t in _tokens:
+                _r_t = _t.replace(".", "\.") + "[\s\S]*?\][\s\S]*?\)"
+                for _match in re.finditer(_r_t, string=_src_txt):
+                    print(_match.group())
+                    _locations_to_inject.append(_match.span()[1])
+            # inject
+            if bool(_locations_to_inject):
+                _start = 0
+                _new_src_txt = ""
+                for _ii in _locations_to_inject:
+                    _new_src_txt += _src_txt[_start: _ii] + ".raise_if_failed()"
+                    _start = _ii
+                _new_src_txt += _src_txt[_locations_to_inject[-1]:]
+            else:
+                _new_src_txt = _src_txt
+
+            # # replace tokens and prefix raise to them
+            # for _t in _tokens:
+            #     _src_txt = _src_txt.replace(_t, "raise " + _t)
+            # # if already done then there will be two raises
+            # # so simple do final replace ;) ... this preserves old changes and add
+            # # raise where we have forgotten
+            # _src_txt = _src_txt.replace("raise raise", "raise")
+            # # finally, write back changes
+            # _fi.write_text(_src_txt, encoding="utf8")
+
+
 
 
 
 if __name__ == "__main__":
     # should_add_raise_explicitly_class_field()
-    add_raise_before_exceptions_to_be_raised_explicitly()
+    # add_raise_before_exceptions_to_be_raised_explicitly()
+    add_raise_if_expected_after_some_exceptions()
