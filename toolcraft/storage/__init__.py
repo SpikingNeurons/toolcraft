afacf5c88f1aab5d439c2487e8e96f538d891362
Praveen Kulkarni <praveenneuron@gmail.com>
2022-03-13 01:40:22 +0100

    no iter

--- toolcraft/storage/__init__.py
+++ toolcraft/storage/__init__.py
@@ -30,7 +30,7 @@ from .file_group import FileGroup, FileGroupFromPaths, NpyMemMap, SHUFFLE_SEED_T
     DETERMINISTIC_SHUFFLE, NO_SHUFFLE, DO_NOT_USE, USE_ALL, \
     SELECT_TYPE, NON_DETERMINISTIC_SHUFFLE, FileGroupConfig
 from .file_group import DownloadFileGroup, NpyFileGroup, TempFileGroup
-from .table import Table, Filter
+from .table import Table, Filter, make_expression
 
 
 # a call so that the CWD FileSystem is loaded if defined in config.toml or else it
