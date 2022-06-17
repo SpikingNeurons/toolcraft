5b60d7d27bc7526c805bc17c24f0f3f765bd9467
prave <praveenneuron@gmail.com>
2022-06-17 02:58:33 +0200

    make children as dict

--- toolcraft/texipy/__base__.py
+++ toolcraft/texipy/__base__.py
@@ -393,14 +393,76 @@ class LaTeX(abc.ABC):
 
 
 @dataclasses.dataclass
-class BeamerFrame(LaTeX):
+class BeamerTableOfContentsFrame(LaTeX):
+    """
+    TableOfContents geared for Beamer ... for normal TableOfContents implement separately
+    """
+
+    title: str = "Outline"
+    hide_all_subsections: bool = False
+    pause_sections: bool = False
+    current_section: bool = False
+
+    # https://tex.stackexchange.com/questions/11100/beamer-atbeginsection-and-insertsubsectionnavigation
+    section_style: str = None  # "show//hide"
+    sub_section_style: str = None  # "hide//show//hide"
+
+    @property
+    def allow_add_items(self) -> bool:
+        """
+        This is specialized frame to hold table of content so we do not allow to add items
+        """
+        return False
 
     @property
     def open_clause(self) -> str:
-        _ret = "% >> frame start " \
-               "\n\\begin{frame}"
+        _ret = "% >> table of contents ... \n" \
+               "\\begin{frame}\n"
         if self.label is not None:
-            _ret += f"\\label{{{self.label}}}"
+            _ret += f"\\label{{{self.label}}}\n"
+        if self.title is not None:
+            _ret += f"\\frametitle{{{self.title}}}%\n"
+
+        # tableofcontents with options ...
+        _toc = "\\tableofcontents["
+        _toc_options = []
+        if self.hide_all_subsections:
+            _toc_options.append("hideallsubsections")
+        if self.pause_sections:
+            _toc_options.append("pausesections")
+        if self.current_section:
+            _toc_options.append("currentsection")
+        if self.section_style is not None:
+            _toc_options.append(f"sectionstyle={self.section_style}")
+        if self.sub_section_style is not None:
+            _toc_options.append(f"subsectionstyle={self.sub_section_style}")
+        _toc = _toc + ",".join(_toc_options) + "]%\n"
+        _ret += _toc
+
+        # final close clause
+        _ret += "\\end{frame}%"
+
+        # return
+        return _ret
+
+    @property
+    def close_clause(self) -> str:
+        return ""
+
+
+@dataclasses.dataclass
+class BeamerFrame(LaTeX):
+
+    title: str = None
+
+    @property
+    def open_clause(self) -> str:
+        _ret = "% >> frame start \n" \
+               "\\begin{frame}\n"
+        if self.label is not None:
+            _ret += f"\\label{{{self.label}}}\n"
+        if self.title is not None:
+            _ret += f"\\frametitle{{{self.title}}}"
         return _ret
 
     @property
@@ -447,6 +509,11 @@ class Beamer(LaTeX):
 
     symbols_file: str = "symbols.tex"
     usepackage_file: str = "usepackage.sty"
+
+    @property
+    @util.CacheResult
+    def labels(self) -> t.List[str]:
+        return []
 
     @property
     def open_clause(self) -> str:
--- toolcraft/texipy/examples/try_beamer.py
+++ toolcraft/texipy/examples/try_beamer.py
@@ -6,7 +6,7 @@ https://latex-beamer.com/quick-start/
 """
 
 
-from toolcraft.texipy import Beamer, Section, Color, SubSection
+from toolcraft.texipy import Beamer, Section, Color, SubSection, BeamerFrame, BeamerTableOfContentsFrame
 from toolcraft.texipy import tikz
 
 
@@ -17,15 +17,23 @@ if __name__ == '__main__':
         title="My Presentation",
         short_title="My Ppt",
         sub_title="With subtitle subtitle ...",
-        author="\nPraveen Kulkarni\\inst{1,2}\n\\and\nPraveen Kulkarni\\inst{1}\n",
+        author="\nPraveen Kulkarni\\inst{1,2}\n\\and\nFirstname Lastname\\inst{1}\n",
         short_author="P. Kulkarni et al.",
-        date="\\today",
+        date="June 21, 2022",
         institute="\n"
-                  "\\inst{1}NXP Semiconductors Germany GmbH \n"
+                  "\\inst{1}Organization One \n"
                   "\\and \n"
-                  "\\inst{2}Radboud University, Nijmegen, Netherlands \n",
+                  "\\inst{2}Organization Two \n",
         symbols_file="symbols.tex",
         usepackage_file="usepackage.sty",
     )
+
+    _ppt.add_item(
+        item=BeamerTableOfContentsFrame(
+            title="Outline", hide_all_subsections=False,
+        )
+    )
+
+    _ppt.add_item(item=BeamerFrame(title="First Frame", label="first_frame").add_item("This is first frame ..."))
 
     _ppt.write(save_to_file="try.tex", make_pdf=True)
