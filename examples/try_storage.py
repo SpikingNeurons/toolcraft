f8aaff405f59430042711fd6c1ed524496cbd335
prave <praveenneuron@gmail.com>
2022-07-31 00:05:04 +0200

    stream logging disabled so that things can work from CLI

--- examples/try_storage.py
+++ examples/try_storage.py
@@ -705,13 +705,13 @@ def try_main():
         util.io_path_delete(_path, force=True)
     _path.mkdir(parents=True, exist_ok=True)
     _path = _path.resolve()
-    # try_hashable_ser()
+    try_hashable_ser()
     try_download_file()
-    # try_auto_hashed_download_file()
-    # try_metainfo_file()
-    # try_creating_folders()
-    # try_arrow_storage(gcs=False)
-    # try_file_storage(gcs=False)
+    try_auto_hashed_download_file()
+    try_metainfo_file()
+    try_creating_folders()
+    try_arrow_storage(gcs=False)
+    try_file_storage(gcs=False)
 
     # todo: support gcs ...
     #   Note: tests cannot work behind firewall for GCS, only use local machine
