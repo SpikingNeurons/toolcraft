a70a8d68b6084fd626d96943dea46e729c2f562d
prave <praveenneuron@gmail.com>
2023-03-18 03:40:52 +0100

    adding files for tches format

--- toolcraft/storage/file_group.py
+++ toolcraft/storage/file_group.py
@@ -269,7 +269,7 @@ class FileGroup(StorageHashable, abc.ABC):
                     ]
                 )
             # look inside path dir
-            for _path in self.path.find(maxdepth=0, detail=False, withdirs=True):
+            for _path in self.path.find(maxdepth=1, detail=False, withdirs=True):
                 if _path.name in self.file_keys and _path.isfile():
                     continue
                 # anything starting with `_` will be ignored
