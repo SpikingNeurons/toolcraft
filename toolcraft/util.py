cf35a147b445cf73a161a75471a57b4f823ef472
pbk0@yoga <praveenneuron@gmail.com>
2024-03-24 16:35:45 +0100

    RE line 685

--- toolcraft/util.py
+++ toolcraft/util.py
@@ -698,7 +698,7 @@ def rhasattr(obj, attr):
     return hasattr(_curr_obj, _nested_attrs[-1])
 
 
-def read_h5py_dataset_efficiently(_data: h5py.Dataset, free_mem_use_frac: float = 0.2, richy_panel = None) -> t.Generator[np.ndarray, None, None]:
+def read_h5py_dataset_efficiently(_data: t.Union[h5py.Dataset, np.ndarray], free_mem_use_frac: float = 0.2, richy_panel = None) -> t.Generator[np.ndarray, None, None]:
     """
     todo: optimize this for automatic batch size ...
       + start with some 10 batches with random sizes form 11th batch select the best batch size from 10 trials ;)
@@ -716,17 +716,17 @@ def read_h5py_dataset_efficiently(_data: h5py.Dataset, free_mem_use_frac: float 
         _index = 0
         while True:
             if richy_panel is not None:
-                richy_panel.update(f"Loading chunk {_index + 1} from h5py.Dataset")
+                richy_panel.update(f"Loading chunk {_index + 1} ")
             _chunk = _data[_index * _num_examples_in_batch: (_index + 1) * _num_examples_in_batch]
             if richy_panel is not None:
-                richy_panel.update(f"Fetching examples from chunk {_index + 1} of h5py.Dataset...")
+                richy_panel.update(f"Fetching examples from chunk {_index + 1} ...")
             _index += 1
             for _ in _chunk:
                 yield _
             if _index * _num_examples_in_batch >= len(_data):
                 break
         if richy_panel is not None:
-            richy_panel.update("Finished reading h5py.Dataset")
+            richy_panel.update("Finished reading ...")
 
     return _gen()
 
