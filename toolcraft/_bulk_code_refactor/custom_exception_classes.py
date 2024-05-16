1b3d96fb96f41f233b147690258b13b5cc59d5be
Praveen Kulkarni <2289914+pbk0@users.noreply.github.com>
2024-05-16 04:46:59 +0200

    utf8 encoding

--- toolcraft/_bulk_code_refactor/custom_exception_classes.py
+++ toolcraft/_bulk_code_refactor/custom_exception_classes.py
@@ -52,12 +52,12 @@ def add_raise_before_exceptions_that_have_no_check():
             # replace tokens and prefix raise to them
             # for _t in _tokens:
             #     _src_txt = _src_txt.replace(_t, "raise " + _t)
-            # # if already done then there will be two raises
-            # # so simply do final replace ;) ... this preserves old changes and add
-            # # raise where we have forgotten
+            # if already done then there will be two raises
+            # so simply do final replace ;) ... this preserves old changes and add
+            # raise where we have forgotten
             # _src_txt = _src_txt.replace("raise raise", "raise")
-            # # finally, write back changes
-            # _fi.write_text(_src_txt, encoding="utf8")
+            # finally, write back changes
+            _fi.write_text(_src_txt, encoding="utf8")
 
 
 def add_raise_if_expected_after_some_exceptions():
